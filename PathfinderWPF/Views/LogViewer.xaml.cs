a2f60e324797e7144df6609f9826baa324654eb0
paul <paul@paulwheeler.com>
2012-06-03 23:08:10 -0400

    -Added Map

--- PathfinderWPF/Views/LogViewer.xaml.cs
+++ PathfinderWPF/Views/LogViewer.xaml.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Pathfinder.WPF.Views {
-	/// <summary>
-	/// Interaction logic for LogViewer.xaml
-	/// </summary>
-	public partial class LogViewer : UserControl {
+
+	/// <summary> Adventure log </summary>
+	public partial class LogViewer {
+
 		public LogViewer() {
 			InitializeComponent();
+			Loaded += LogViewer_Loaded;
 		}
-	}
+
+		void LogViewer_Loaded(object sender, RoutedEventArgs e) {
+			Log.Items.Add(new ListBoxItem { Content = "test2" });
+		}
+
+	};
+
 }
--- PathfinderWPF/Views/MapViewer.xaml.cs
+++ PathfinderWPF/Views/MapViewer.xaml.cs
@@ -1,24 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Pathfinder.Domain;
 
 namespace Pathfinder.WPF.Views {
-	/// <summary>
-	/// Interaction logic for MapViewer.xaml
-	/// </summary>
-	public partial class MapViewer : UserControl {
+
+	/// <summary> Views a map </summary>
+	public partial class MapViewer {
+
+		public Room Model { get; set; }
+
 		public MapViewer() {
 			InitializeComponent();
+
+			Model = new Room { Width = 10, Height = 10 };
+			DrawMap();
 		}
-	}
+
+		void DrawMap() {
+			Tiles.Children.Clear();
+			for (var c = 0; c < Model.Width; c++) {
+				Tiles.ColumnDefinitions.Add(new ColumnDefinition());
+			}
+			for (var r = 0; r < Model.Width; r++) {
+				Tiles.RowDefinitions.Add(new RowDefinition());
+			}
+			for (var c = 0; c < Model.Width; c++) {
+				for (var r = 0; r < Model.Width; r++) {
+					var tile = new Rectangle();
+					tile.Fill = new SolidColorBrush(Colors.Black);
+					tile.Margin = new Thickness(1); ;
+					tile.SetValue(Grid.RowProperty, r);
+					tile.SetValue(Grid.ColumnProperty, c);
+					Tiles.Children.Add(tile);
+				}
+			}
+		}
+
+	};
+
 }
